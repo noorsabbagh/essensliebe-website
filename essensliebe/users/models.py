b652c96e741acb679e328c67894a9d65aca95a8e
Noor Sabbagh <noorsabbagh77@gmail.com>
2018-08-06 14:49:49 +1000

    Added __str__ method to User Model

--- essensliebe/users/models.py
+++ essensliebe/users/models.py
@@ -7,3 +7,6 @@ class users(models.Model):
     street = models.CharField(max_length=255)
     suburb = models.CharField(max_length=255)
     city =models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
