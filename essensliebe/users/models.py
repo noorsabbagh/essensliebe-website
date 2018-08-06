171033e8e3f1e662be04aec9ead5d88fc55f6879
Darcy <s3600902@student.rmit.edu.au>
2018-08-06 14:42:22 +1000

    Created User Model

--- essensliebe/users/models.py
+++ essensliebe/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class users(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    suburb = models.CharField(max_length=255)
+    city =models.CharField(max_length=255)
