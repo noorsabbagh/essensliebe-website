1b43fbd4a2a00a8656e41cf3c22dc5313058578b
Noor Sabbagh <noorsabbagh77@gmail.com>
2018-08-06 15:39:48 +1000

    HTML Page Added to Template Folder

--- essensliebe/webapp/views.py
+++ essensliebe/webapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>This is the index page</h1>")
+    return render(request, 'webapp/index.html')
