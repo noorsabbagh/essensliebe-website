85eec1da6affea3806fa46217d94e9d50ea2215f
Noor Sabbagh <noorsabbagh77@gmail.com>
2018-08-07 14:01:28 +1000

    Index Page Complete + Login Page Template Created

--- essensliebe/essensliebe/urls.py
+++ essensliebe/essensliebe/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('webapp.urls'),)
+    path('index/', include('webapp.urls')),
+    path('login/', include('login.urls')),
 ]
--- essensliebe/login/views.py
+++ essensliebe/login/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+def login(request):
+    return render(request, 'login/login.html')
