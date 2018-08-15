29af84acf473ebdd2d4b837e7759c4d3d7666249
Elyssa Mendoza <elyssa01.m@gmail.com>
2018-08-15 14:42:33 +1000

    Created preferences form

--- essensliebe/preferences/views.py
+++ essensliebe/preferences/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 from django.conf import settings
 from django.contrib.auth.models import User
 from .models import Questions
 # Create your views here.
 def questions(request):
-    queryset = Questions.objects.all().order_by('-timestamp')
-    context = {
-        "queryset": queryset
-    }
-    return render(request, "questions.html", context)
+    if request.user.is_authenticated():
+        queryset = Questions.objects.all().order_by('-timestamp')
+        context = {
+            "queryset": queryset
+        }
+        return render(request, "questions.html", context)
+    else:
+        raise Http404
