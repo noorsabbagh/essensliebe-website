7f31a2662a86d591d7db69127411562abe37a73c
Elyssa Mendoza <elyssa01.m@gmail.com>
2018-08-15 15:10:33 +1000

    Created preferences HTML page

--- essensliebe/preferences/views.py
+++ essensliebe/preferences/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render, Http404
 from django.conf import settings
 from django.contrib.auth.models import User
 from .models import Questions
+from .forms import UserResponseForm
+
 # Create your views here.
 def questions(request):
     if request.user.is_authenticated():
+        form = UserResponseForm(request.POST or None)
+        if form.is_valid():
+            print(form.cleaned_data)
         queryset = Questions.objects.all().order_by('-timestamp')
+        instance = queryset[0]
         context = {
-            "queryset": queryset
+            "form":form,
+            "instance":instance
         }
         return render(request, "questions.html", context)
     else:
-        raise Http404
+        raise Http404 
