021754ccb2bc1f2b41aa9f8d406f28ee53f34cbb
Nick <nicnick1989@gmail.com>
2018-08-20 15:48:45 +1000

    Added UserAnswer Model

--- essensliebe/questions/models.py
+++ essensliebe/questions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Question(models.Model):
@@ -19,3 +20,23 @@ class Answer(models.Model):
 
     def __str__(self):
         return(self.text)
+
+
+LEVELS = (
+        ('Mandatory', 'Mandatory'),
+        ('Very Important', 'Very Important'),
+        ('Somewhat Important', 'Somewhat Important'),
+        ('Not Important', 'Not Importan'),
+        )
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    my_answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='user_answer')
+    my_answer_importance = models.CharField(max_length=50, choices=LEVELS)
+    their_answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE, related_name='match_answer')
+    their_importance = models.CharField(max_length=50, choices=LEVELS)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.my_answer
