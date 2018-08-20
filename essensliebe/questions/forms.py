7bbcb75a71cd0599f1eaa79909077d28e8cd65b5
Nick <nicnick1989@gmail.com>
2018-08-20 15:19:18 +1000

    Create Questions Form

--- essensliebe/questions/forms.py
+++ essensliebe/questions/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 
+LEVELS = (
+        ('Mandatory', 'Mandatory'),
+        ('Very Important', 'Very Important'),
+        ('Somewhat Important', 'Somewhat Important'),
+        ('Not Important', 'Not Importan'),
+        )
+
 class UserResponseForm(forms.Form):
     question_id = forms.IntegerField()
     answer_id = forms.IntegerField()
+    importance_level = forms.ChoiceField(choices=LEVELS)
+    partner_answer_id = forms.IntegerField()
+    partner_importance_level = forms.ChoiceField(choices=LEVELS)
