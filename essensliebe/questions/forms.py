b01ed4b0d66af80881fa84fc87d123faa7626488
Noor Sabbagh <noorsabbagh77@gmail.com>
2018-08-20 17:44:58 +1000

    Created form validation for UserAnswer

--- essensliebe/questions/forms.py
+++ essensliebe/questions/forms.py
@@ -1,11 +1,5 @@
 from django import forms
-
-LEVELS = (
-        ('Mandatory', 'Mandatory'),
-        ('Very Important', 'Very Important'),
-        ('Somewhat Important', 'Somewhat Important'),
-        ('Not Important', 'Not Importan'),
-        )
+from .models import LEVELS, Answer, Question
 
 class UserResponseForm(forms.Form):
     question_id = forms.IntegerField()
@@ -13,3 +7,27 @@ class UserResponseForm(forms.Form):
     importance_level = forms.ChoiceField(choices=LEVELS)
     their_answer_id = forms.IntegerField()
     their_importance_level = forms.ChoiceField(choices=LEVELS)
+    
+    def clean_question_id(self):
+        question_id = self.cleaned_data.get('question_id')
+        try:
+            obj = Question.objects.get(id=question_id)
+        except:
+            raise forms.ValidationError('There was an error with the question. Please retry.')
+        return question_id
+
+    def clean_answer_id(self):
+        answer_id = self.cleaned_data.get('answer_id')
+        try:
+            obj = Answer.objects.get(id=answer_id)
+        except:
+            raise forms.ValidationError('There was an error with the answer. Please retry.')
+        return answer_id
+
+    def clean_their_answer_id(self):
+        their_answer_id = self.cleaned_data.get('their_answer_id')
+        try:
+            obj = Answer.objects.get(id=their_answer_id)
+        except:
+            raise forms.ValidationError('There was an error with the answer you have provided for them. Please retry.')
+        return their_answer_id
