ecf1be2a2ddd9fe53f121675a05d9ee40e7dfdb7
Noor Sabbagh <noorsabbagh77@gmail.com>
2018-08-20 16:24:33 +1000

    Changed Variable Names in Forms

--- essensliebe/questions/forms.py
+++ essensliebe/questions/forms.py
@@ -11,5 +11,5 @@ class UserResponseForm(forms.Form):
     question_id = forms.IntegerField()
     answer_id = forms.IntegerField()
     importance_level = forms.ChoiceField(choices=LEVELS)
-    partner_answer_id = forms.IntegerField()
-    partner_importance_level = forms.ChoiceField(choices=LEVELS)
+    their_answer_id = forms.IntegerField()
+    their_importance_level = forms.ChoiceField(choices=LEVELS)
