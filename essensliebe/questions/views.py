48ab9f42091d44c3189f2b4992d7c1896fbbc76b
Noor Sabbagh <noorsabbagh77@gmail.com>
2018-08-20 12:35:50 +1000

    Created View for Questions

--- essensliebe/questions/views.py
+++ essensliebe/questions/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, Http404
+from .models import Question
 # Create your views here.
+def questions_view(request):
+    if request.user.is_authenticated():
+        queryset = Question.objects.all().order_by('-timestamp')
+        context = {
+            "queryset": queryset
+        }
+        return render(request, "questions.html", context)
+    else:
+        raise Http404
