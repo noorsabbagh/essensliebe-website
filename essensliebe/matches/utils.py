900ee8f2e77fd099af92913f3e147aa8328e4574
Nick <nicnick1989@gmail.com>
2018-08-21 16:39:32 +1000

    Finalised Algorithm

--- essensliebe/matches/utils.py
+++ essensliebe/matches/utils.py
@@ -1,13 +1,48 @@
 from decimal import Decimal
 from django.contrib.auth import get_user_model
+from django.db.models import Q
+from questions.models import UserAnswer, Question
 
-from questions.models import UserAnswer
+def get_match(user_a, user_b):
+    q1 = Q(useranswer__user=user_a)
+    q2 = Q(useranswer__user=user_b)
+    question_set = Question.objects.filter(q1 | q2).distinct()
+    a_points = 0
+    b_points = 0
+    a_total_points = 0
+    b_total_points = 0 
+    questions_in_common = 0 
+    for question in question_set:
+        try:
+            a = UserAnswer.objects.get(user=user_a, question=question)
+        except:
+            a = None
+        try:
+            b = UserAnswer.objects.get(user=user_b, question=question)
+        except:
+            b = None
+        if a and b:
+            questions_in_common = 1
+            if a.their_answer == b.my_answer:
+                b_points += a.their_points
+            b_total_points += a.their_points
 
-User = get_user_model()
+            if b.their_answer== a.my_answer:
+                a_points += b.their_points
+            a_total_points += b.their_points
 
-users = User.objects.all()
-all_user_answers = UserAnswer.objects.all().order_by("user__id")
+    if questions_in_common > 0: 
+        a_decimal = a_points / Decimal(a_total_points)
+        b_decimal = b_points / Decimal(b_total_points)
+        if a_decimal == 0:
+            a_decimal = 0.000001
+        if b_decimal == 0:
+            a_decimal = 0.000001
+        match_percentage = (Decimal(a_decimal)) * Decimal(b_decimal)) ** (1/Decimal(questions_in_common))
+        return match_percentage,questions_in_common
 
+    else:
+        return 0.0, 0
 
 def get_points(user_a, user_b):
     a_answers = UserAnswer.objects.filter(user=user_a)[0]
@@ -30,12 +65,10 @@ def get_points(user_a, user_b):
                 assigning total points
                 '''
                 a_points_possible += a.their_points
-            print "%s has awarded %s points of %s to %s" %(user_a, a_total_awarded, a_points_possible, user_b)
+            print ("%s has awarded %s points of %s to %s" % (user_a, a_total_awarded, a_points_possible, user_b))
     percent = a_total_awarded / Decimal(a_points_possible)
-    print percent, num_question
+    print (percent, num_question)
     if percent == 0:
         percent = 0.00001
     return percent, num_question
 
-match_percentage = "%.2f" % ((Decimal(a[0]) * Decimal(b[0])) ** (1/Decimal(b[1])))
-print match_percentage
