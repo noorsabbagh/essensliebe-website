ea8088244e547c48d7fdced76ff463373b69f0f6
Elyssa Mendoza <elyssa01.m@gmail.com>
2018-08-22 15:46:57 +1000

    Created Matches View

--- essensliebe/matches/views.py
+++ essensliebe/matches/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Match
 # Create your views here.
+def matches_view(request):
+    if request.user.is_authenticated:
+        matches = Match.objects.get_matches_with_percent(request.user)
+        context = {
+            "matches": matches,
+        }
+        return render(request, "matches_dashboard.html", context)
+    return render(request, "index", context)
