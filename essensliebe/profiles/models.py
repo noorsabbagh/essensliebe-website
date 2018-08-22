dd214f70935ec02a9a24d7a5dc15509786f77188
Elyssa Mendoza <elyssa01.m@gmail.com>
2018-08-22 15:46:34 +1000

    Added get_absolute_url method to profile

--- essensliebe/profiles/models.py
+++ essensliebe/profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-
+from django.urls import reverse
 # Create your models here.
 
 class Profile(models.Model):
@@ -12,6 +12,13 @@ class Profile(models.Model):
     ethnicity = models.CharField(max_length=10, null=True, blank=True)
     description = models.CharField(max_length=500, null=True, blank=True)
     picture = models.ImageField(upload_to="profile_image/", default="/media/profile_image/def_image.png")
+
+    def __str__(self):
+        return self.user.username 
+    
+    def get_absolute_url(self):
+        url = reverse("profile", kwargs={"username": self.user.username})
+        return url
 
 def create_profile(sender, **kwargs):
 	if kwargs ['created']:
