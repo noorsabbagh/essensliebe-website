5147e377b20050c83d8438c493d77a379679676c
tsang <chocolate2395@gmail.com>
2018-08-23 15:49:10 +1000

    modified settings and urls

--- essensliebe/essensliebe/urls.py
+++ essensliebe/essensliebe/urls.py
@@ -18,10 +18,12 @@ from django.urls import path, include
 from account.views import (login_view, register_view, logout_view)
 from profiles.views import profile, edit_profile, edit_food_prefrences, edit_partner_prefrences, prefrences
 from questions.views import questions_view, single
+from directmessage.views import inbox, sent, compose, view_direct_message, reply
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from matches.views import matches_view
+from likes.views import like_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,12 +33,19 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('questions/', questions_view, name='questions'),
     path('questions/<int:id>/', single, name='questions_single'),
+    path('directmessage/inbox', inbox, name='inbox'),
+    path('directmessage/sent', sent, name='sent'),
+    path('directmessage/compose', compose, name='compose'),
+    path('directmessage/view/<int:dm_id>/', view_direct_message, name='view_direct_message'),
+    path('directmessage/view/<int:dm_id>/reply/', reply, name='reply'),
     path('profile/<str:username>/', profile, name='profile'),
     path('profile/<str:username>/edit/', edit_profile, name='edit_profile'),
     path('profile/<str:username>/prefrences', prefrences, name='prefrences'),
     path('profile/<str:username>/prefrences/partner', edit_partner_prefrences, name='edit_partner_prefrences'),
     path('profile/<str:username>/prefrences/food', edit_food_prefrences, name='edit_food_prefrences'),
-    path('matches/', matches_view, name='matches_view'), 
+    path('matches/', matches_view, name='matches_view'),
+    path('like/<int:id>/', like_user, name='like_user'),
+
 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
