d2b9de7527fafd14ed222af326eeb6975978c50c
tsang <chocolate2395@gmail.com>
2018-08-23 15:47:01 +1000

    Implement message functionality

---
+++ essensliebe/directmessage/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import DirectMessage
+
+class ComposeForm(forms.ModelForm):
+
+    class Meta:
+        model = DirectMessage
+        fields = ('receiver', 'subject', 'body')
+        widgets = {
+                'body': forms.Textarea(attrs={'cols':80, 'row': 20}),
+            }
+
+class ReplyForm(forms.ModelForm):
+    class Meta:
+        model = DirectMessage
+        fields = ('body', )
+        widgets = {
+                'body': forms.Textarea(attrs={'cols':80, 'row': 20}),
+            }
