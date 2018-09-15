8da5cba6f6ca2ff47ec1512c26bc25f4bb975c3a
Darcy <s3600902@student.rmit.edu.au>
2018-09-15 17:59:20 +1000

    Added some css'ing and testing dashboard functionality

--- essensliebe/webapp/views.py
+++ essensliebe/webapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Match
 
 
 # Create your views here.
 def index(request):
     return render(request, 'webapp/index.html')
+	
+def matches_view(request):
+    if request.user.is_authenticated:
+        matches = Match.objects.get_matches_with_percent(request.user)
+        context = {
+            "matches": matches,
+        }
+        return render(request, "matches_dashboard.html", context)
+    return render(request, "index", context)
