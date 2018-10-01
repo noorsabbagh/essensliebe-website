8d0824e2aaeb7057a1cccb823ef7d260159f3da4
Noor Sabbagh <noorsabbagh77@gmail.com>
2018-10-01 21:16:43 +1000

    Created google maps api call

--- essensliebe/dating/localisation.py
+++ essensliebe/dating/localisation.py
@@ -3,9 +3,14 @@ from geopy.geocoders import Nominatim
 from geopy.distance import great_circle
 from geographiclib.geodesic import Geodesic
 from diblogeo import Geo
+import requests, json
+from googleplaces import GooglePlaces
 
 # Creates new geolocator object using Nominatim.
 geolocator = Nominatim(user_agent='app_demo')
+# google places api key
+api_key = 'AIzaSyCXmfyVfwPypP1HwUnVE1LMKbqqz6heipk'
+google_places = GooglePlaces(api_key)
 
 # Method to convert address to geolocation, takes address parameter, returns long and lat.
 def address_to_geolocation(address):
@@ -36,3 +41,24 @@ def centre_point_calc(lt1,ln1,b,d):
     point = Geo((lt1, ln1))
     destination = point.destination(bearing=b, kilometers=d)
     return(destination)
+
+# Finds nearby restaurants by using location, radius and types parameters.
+# Location: lat and long of the centre point between the matched users.
+# Radius: the radius is half of the distance between the matched users.
+# Types: the type of etablishment to be searched, such as 'restaurant'.
+def find_nearby_places(lat, lon, radius):
+    AUTH_KEY = api_key
+    #LOCATION = str(lat) + "," + str(lon)
+    LOCATION = "-37.758995953831196,144.83652518592598"
+    RADIUS = radius
+    TYPES = 'restaurant'
+    MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+            '?location=%s'
+            '&radius=%s'
+            '&type=%s'
+            '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
+    # Grabs the JSON result
+    print(MyUrl)
+    response = requests.get(MyUrl)
+    json_data = json.loads(response.text)
+    return(json_data)
