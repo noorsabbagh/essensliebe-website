13b300b0f91f765b95a585fc0a4cc3587882a840
Darcy <s3600902@student.rmit.edu.au>
2018-11-04 22:30:59 +1100

    Added tests to Preferences
    Added the required tests as well as fixing issues with validation

--- essensliebe/profiles/forms.py
+++ essensliebe/profiles/forms.py
@@ -32,22 +32,66 @@ class EditProfileForm(forms.ModelForm):
 		}   
 		
 class EditPartnerPrefrencesForm(forms.ModelForm):
-    class Meta:
-        model = PartnerPrefrences
-        fields = (
+
+	age = forms.IntegerField(required=True,widget=forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[0-9 ]+', 'title':'Enter Characters Only '}))
+	sex = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[A-Za-z ]+', 'title':'Enter Characters Only '}))
+	location = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[\w ]+', 'title':'Enter Characters Only '}))
+	ethnicity = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[A-Za-z ]+', 'title':'Enter Characters Only '}))
+	def clean(self):
+		form_data = self.cleaned_data
+		age = self.cleaned_data.get('age')
+		sex = self.cleaned_data.get('sex')
+		location = self.cleaned_data.get('location')
+		ethnicity = self.cleaned_data.get('ethnicity')
+		if age is None or not 18 < age < 112:
+			self.errors["age"] = ["Your age must be a identified by a number between 18 and 112"]
+		if sex is None:
+			self.errors["sex"] = ["Your Sex must be identified in lower case male, female, or other"]
+		return form_data
+		if location is None:
+			self.errors["location"] = ["Make your location a valid address e.g 35 Swanston Street Melbourne"]
+		return form_data
+		if ethnicity is None:
+			self.errors["ethnicity"] = ["Ensure you enter a valid Race you belong to"]
+		return form_data
+	class Meta:
+		model = PartnerPrefrences
+		fields = {
             'location',
             'sex',
             'age',
             'ethnicity'
-        )
+        }
 
 
 
 
 class EditFoodPrefrencesForm(forms.ModelForm):
-    class Meta:
-        model = FoodPrefrences
-        fields = (
+	
+
+	fav_food = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[A-Za-z ]+', 'title':'Enter Characters Only '}))
+	vegan  = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[A-Za-z ]+', 'title':'Enter Characters Only '}))
+	vegetarian = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[A-Za-z ]+', 'title':'Enter Characters Only '}))
+	def clean(self):
+		form_data = self.cleaned_data
+		fav_food = self.cleaned_data.get('fav_food')
+		vegan = self.cleaned_data.get('vegan')
+		vegetarian= self.cleaned_data.get('vegetarian')
+		if fav_food is None:
+			self.errors["fav_food"] = ["Please make sure you select a favorite food"]
+		return form_data
+		if vegan is None:
+			self.errors["vegan"] = ["Please make sure your are either vegan or not"]
+		return form_data
+		if vegetarian is None:
+			self.errors["vegetarian"] = ["Please make sure you are either vegetarian or not"]
+		return form_data
+	
+	
+	
+	class Meta:
+		model = FoodPrefrences
+		fields = (
             'fav_food',
             'vegan',
             'vegetarian'
@@ -63,8 +107,8 @@ class EditFoodPrefrencesForm(forms.ModelForm):
         #        re.search('[A-Za-z]', location)
         #            raise forms.ValidationError ("Location must only use Letters no characters")
 		#	    return location 
-        def clean_ethnicity(self):
-                ethnicity = self.clean_data.get(ethnicity)
-                ethnicity = re.search('[A-Za-z]', ethnicity)
+        #def clean_ethnicity(self):
+                #ethnicity = self.clean_data.get(ethnicity)
+                #ethnicity = re.search('[A-Za-z]', ethnicity)
                 
                     
--- essensliebe/profiles/tests.py
+++ essensliebe/profiles/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from .models import PartnerPrefrences, FoodPrefrences
+from .forms import *
 
-# Create your tests here.
+class Setup_Class(TestCase):
+	
+	def setUp(self):
+		self.user = User.objects.create(username="testyMcTesty" ,password="pass1234")
+
+		
+class UserPreferencesForm_Test(TestCase):
+	
+	def test_UserPreferencesForm_valid(self):
+		 form = EditPartnerPrefrencesForm(data={"location":"35 boronia road boronia","sex":"male","age":"24","ethnicity":"white"})
+		 self.assertTrue(form.is_valid())
+
+	def test_UserPreferencesForm_Invalid_Age_Young(self):
+		 form = EditPartnerPrefrencesForm(data={"location":"35 boronia road boronia","sex":"male","age":"2"})
+		 self.assertFalse(form.is_valid())	 
+
+	def test_UserPreferencesForm_Invalid_Age_Old(self):
+		 form = EditPartnerPrefrencesForm(data={"location":"35 boronia road boronia","sex":"male","age":"260"})
+		 self.assertFalse(form.is_valid())
+		
+	def test_UserPreferencesForm_Missing_Age(self):
+		 form = EditPartnerPrefrencesForm(data={"location":"35 boronia road boronia","sex":"male","age":""})
+		 self.assertFalse(form.is_valid())
+		 
+	def test_UserPreferencesForm_Missing_Sex(self):
+		 form = EditPartnerPrefrencesForm(data={"location":"35 boronia road boronia","sex":"","age":"21"})
+		 self.assertFalse(form.is_valid())
+		 
+	def test_UserPreferencesForm_Missing_Location(self):
+		 form = EditPartnerPrefrencesForm(data={"location":"","sex":"male","age":"21"})
+		 self.assertFalse(form.is_valid())
+	
+	def test_UserPreferencesForm_Missing_Ethnicity(self):
+		 form = EditPartnerPrefrencesForm(data={"location":"35 boronia road boronia","sex":"male","age":"24","ethnicity":""})
+		 self.assertFalse(form.is_valid())
+		 
+		 
+class FoodPreferencesForm_Test(TestCase):
